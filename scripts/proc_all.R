f5c907e0831aff75e09db0ad55b061c5f9c9f157
Alan Duanmu <alan.duanmu@gmail.com>
2016-08-05 14:32:29 -0700

    Don't need the subdirectory stuff

--- scripts/proc_all.R
+++ scripts/proc_all.R
@@ -14,17 +14,12 @@ setwd(DATA_PATH)
 
 demographics_file = "~/Downloads/Sea Crest Demographics.xlsx"
 demographics = load_ace_demographics(demographics_file)
-
-# load and process each subdirectory individually
-
-datasets = list.dirs(recursive = FALSE)
-for (dset in datasets) {
   
-  # load and process
-  dat = load_ace_bulk(path = dset)
-  proc = proc_by_module(dat, verbose = TRUE)
-  proc_demo = lapply(proc, function(x){
-    merge(x, demographics, by = "pid", all = FALSE)
-  })
-}
+# load and process
+dat = load_ace_bulk(path = DATA_PATH)
+proc = proc_by_module(dat, verbose = TRUE)
+proc_demo = lapply(proc, function(x){
+  merge(x, demographics, by = "pid", all = FALSE)
+})
+
 
