3b52f5386d9c11d7a0c758e9efa6557b0a06309c
monicathieu <mthieu@stanford.edu>
2016-09-27 22:38:24 -0700

    expects demographic data in new format (aceR 1.2.3)

--- R/clean_grade_gender.R
+++ R/clean_grade_gender.R
@@ -2,15 +2,10 @@ require(plyr) # the calling markdown must have these loaded already
 require(dplyr)
 clean_grade_gender = function(df) {
   df %>%
-    mutate(grade = as.factor(grade),
-           gender = as.factor(gender),
-           grade_num = NA) %>%
-    within({
-      grade_num[grade == "3rd Grade"] = 3
-      grade_num[grade == "5th Grade"] = 5
-      grade_num[grade == "7th Grade"] = 7
-    }) %>%
-    mutate(grade_num = grade_num - 3) %>%
+    mutate(grade = as.numeric(grade),
+           grade_label = as.factor(paste("Grade", grade)),
+           grade = grade - min(grade), # center to the YOUNGEST grade = 0
+           gender = as.factor(gender)) %>%
     within({
       contrasts(gender) = c(-1, 1)
     })
