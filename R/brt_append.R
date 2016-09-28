0a014b2ee2ba3484940555059f049c6f32f6c63a
monicathieu <mthieu@stanford.edu>
2016-09-27 22:37:56 -0700

    now expects data formatted using aceR 1.2.3 (min)

--- R/brt_append.R
+++ R/brt_append.R
@@ -1,10 +1,8 @@
 require(plyr) # the calling markdown must have these loaded already
 require(dplyr)
 
-brt_append = function(df) {
-  df %>%
-    select(pid, time, gender:intervention_type, everything()) %>%
-    select(-bid, -file) %>%
+brt_append = function(df, ext_demo_data = FALSE) {
+  df <- df %>%
     left_join(
       select(brt,
              pid,
@@ -14,6 +12,11 @@ brt_append = function(df) {
              brt_rt_median.right = rt_median.right,
              brt_rt_mean.left = rt_mean.left,
              brt_rt_median.left = rt_median.left),
-      by = c("pid" = "pid")) %>%
-    select(pid:intervention_type, starts_with("brt"), everything())
-}
+      by = c("pid" = "pid"))
+  if(ext_demo_data) { df <- df %>%
+    select(pid, time, gender:intervention_type, starts_with("brt"), everything())
+  } else {
+    df <- df %>%
+      select(pid, age:time, starts_with("brt"), everything())
+  }
+  return(df)}
