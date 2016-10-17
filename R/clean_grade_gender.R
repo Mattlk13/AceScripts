316184b99a6632bb50c93c92611171cf2be1be7c
monicathieu <mthieu@stanford.edu>
2016-10-17 15:52:09 -0700

    comment to explain gender effect coding

--- R/clean_grade_gender.R
+++ R/clean_grade_gender.R
@@ -7,6 +7,6 @@ clean_grade_gender = function(df) {
            grade = grade - min(grade), # center to the YOUNGEST grade = 0
            gender = as.factor(gender)) %>%
     within({
-      contrasts(gender) = c(-1, 1)
+      contrasts(gender) = c(-1, 1) # females, then males (expect females to be FASTER)
     })
 }
