eab9c189ab98a922083393b6cafdb8f3d81532eb
monicathieu <mthieu@stanford.edu>
2017-01-19 22:50:19 -0800

    account for dominant and non-dominant labeled data

--- R/brt_append.R
+++ R/brt_append.R
@@ -1,17 +1,15 @@
 require(plyr) # the calling markdown must have these loaded already
 require(dplyr)
 
-brt_append = function(df, ext_demo_data = FALSE) {
+brt_append = function(df, brt, ext_demo_data = FALSE) {
   df <- df %>%
     left_join(
       select(brt,
              pid,
              brt_rt_mean.overall = rt_mean.overall,
              brt_rt_median.overall = rt_median.overall,
-             brt_rt_mean.right = rt_mean.right,
-             brt_rt_median.right = rt_median.right,
-             brt_rt_mean.left = rt_mean.left,
-             brt_rt_median.left = rt_median.left),
+             brt_rt_mean.dominant = rt_mean.dominant,
+             brt_rt_mean.nondominant = rt_mean.nondominant),
       by = c("pid" = "pid"))
   if(ext_demo_data) { df <- df %>%
     select(pid, time, gender:intervention_type, starts_with("brt"), everything())
