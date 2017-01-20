c53c1f9446b95101ad6393177559707fc4d15840
monicathieu <mthieu@stanford.edu>
2017-01-19 22:49:24 -0800

    clean for mis-formatted demographic info

--- R/clean_grade_gender.R
+++ R/clean_grade_gender.R
@@ -1,10 +1,17 @@
 require(plyr) # the calling markdown must have these loaded already
 require(dplyr)
 clean_grade_gender = function(df) {
-  df %>%
+  df = df %>%
     mutate(grade = as.numeric(grade),
            grade_label = as.factor(paste("Grade", grade)),
-           grade = grade - min(grade), # center to the YOUNGEST grade = 0
+           grade_label = na_if(grade_label, "Grade NA"),
+           grade = grade - min(grade, na.rm = T), # center to the YOUNGEST grade = 0
+           age = na_if(age, ""),
+           age = na_if(age, "?"),
+           handedness = na_if(handedness, ""),
+           handedness = na_if(handedness, "?"),
+           gender = na_if(gender, ""),
+           gender = na_if(gender, "?"),
            gender = as.factor(gender)) %>%
     within({
       contrasts(gender) = c(-1, 1) # females, then males (expect females to be FASTER)
